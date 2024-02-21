6f3a31896f846025638d719b4bd8c4bcfa550925
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-02-21 19:04:20 +0900

    add: rounded segments for snake body

--- main.cpp
+++ main.cpp
@@ -19,9 +19,11 @@ class Snake {
 
   void Draw() {
     for (unsigned int i = 0; i < body.size(); i++) {
-      int x = body[i].x;
-      int y = body[i].y;
-      DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, darkGreen);
+      float x = body[i].x;
+      float y = body[i].y;
+      Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize,
+                                    (float)cellSize};
+      DrawRectangleRounded(segment, 0.5, 6, darkGreen);
     }
   }
 };
