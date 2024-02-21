cac40e1e7f9aa60da382b5cd75a5aa377728c2a7
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-02-21 18:58:11 +0900

    fix: need unsigned int in for loop

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ class Snake {
   std::deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
 
   void Draw() {
-    for (int i = 0; i < body.size(); i++) {
+    for (unsigned int i = 0; i < body.size(); i++) {
       int x = body[i].x;
       int y = body[i].y;
       DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, darkGreen);
